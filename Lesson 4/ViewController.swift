2a6d0c43129f857e0e9edbec53d46d1ce3eae1d2
Тигран Левонян <hp6x5nywzz@privaterelay.appleid.com>
2021-06-23 23:52:56 +0300

    no message

---
+++ Lesson 4/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Lesson 4
+//
+//  Created by T1GER on 23.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func billAction(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
